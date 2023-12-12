3809fa925c716c303cd4a55fc9575d0dc179bbf8
Andry <andrych17@gmail.com>
2023-12-12 11:58:10 +0800

    [Andry] - Fixed UI

--- resources/views/components/ui-text-field.blade.php
+++ resources/views/components/ui-text-field.blade.php
@@ -1,21 +1,20 @@
 <div class="mb-3 responsive-field" @if(isset($span)) span="{{ $span }}"@endif @if(isset($visible) && $visible === 'false') style="display: none;" @endif>
     @if (!empty($label))
-        <label for="customInput" class="form-label {{ isset($required) && $required === 'true' ? 'required' : '' }}">
-            {{ $label }} :
-        </label>
+        <div class="responsive-label">
+            <label class="{{ isset($required) && $required === 'true' ? 'required' : '' }}">{{ $label }} :</label>
+        </div>
     @endif
 
     <div class="responsive-input-container">
         @if(isset($type) && $type === 'textarea')
-            <textarea id="customInput" wire:model.defer="{{ $model }}" rows="5" class="form-control @error($model) is-invalid @enderror" @if(isset($action) && $action == 'View' || (!empty($enabled) && $enabled === 'false')) disabled @endif @if(isset($required) && $required === 'true') required @endif placeholder="{{ isset($placeHolder) ? $placeHolder : '' }}" wire:change="{{ isset($onChanged) ? $onChanged : '' }}"></textarea>
-            @error($model)
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+            <textarea wire:model.defer="{{ $model }}" rows="5" class="responsive-textarea form-control @error($model) is-invalid @enderror" @if(isset($action) && $action == 'View' || (!empty($enabled) && $enabled === 'false')) disabled @endif @if(isset($required) && $required === 'true') required @endif placeholder="{{ isset($placeHolder) ? $placeHolder : '' }}" wire:change="{{ isset($onChanged) ? $onChanged : '' }}"></textarea>
         @else
-            <input id="customInput" wire:model.defer="{{ $model }}" type="{{ isset($type) ? $type : 'text' }}" class="form-control @error($model) is-invalid @enderror" @if(isset($action) && $action == 'View' || (!empty($enabled) && $enabled === 'false')) disabled @endif @if(isset($required) && $required === 'true') required @endif placeholder="{{ isset($placeHolder) ? $placeHolder : '' }}" autocomplete="{{ isset($type) && $type === 'password' ? 'new-password' : 'off' }}" wire:change="{{ isset($onChanged) ? $onChanged : '' }}" />
-            @error($model)
-                <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
+            <input wire:model.defer="{{ $model }}" type="{{ isset($type) ? $type : 'text' }}" class="responsive-input form-control @error($model) is-invalid @enderror" @if(isset($action) && $action == 'View' || (!empty($enabled) && $enabled === 'false')) disabled @endif @if(isset($required) && $required === 'true') required @endif placeholder="{{ isset($placeHolder) ? $placeHolder : '' }}" autocomplete="{{ isset($type) && $type === 'password' ? 'new-password' : 'off' }}" wire:change="{{ isset($onChanged) ? $onChanged : '' }}" />
         @endif
+
+        @error($model)
+            <div class="invalid-feedback">{{ $message }}</div>
+        @enderror
     </div>
 </div>
+
