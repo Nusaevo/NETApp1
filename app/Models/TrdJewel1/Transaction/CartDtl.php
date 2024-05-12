436e7df7efeaa9b59026824831edb04d8dbb0af9
Andry <andrych17@gmail.com>
2024-05-12 21:24:50 +0800

    [Andry] - Commit env.local

---
+++ app/Models/TrdJewel1/Transaction/CartDtl.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\TrdJewel1\Transaction;
+
+use App\Models\Base\BaseModel;
+
+class CartDtl extends BaseModel
+{
+    protected static function boot()
+    {
+        parent::boot();
+    }
+
+    protected $fillable = [
+        'trhdr_id',
+        'tr_seq',
+        'matl_id',
+        'matl_code',
+        'matl_descr',
+        'matl_uom',
+        'qty',
+        'qty_reff',
+        'price',
+        'amt'
+    ];
+
+    public function scopeGetByCartHdr($query, $id)
+    {
+        return $query->where('trhdr_id', $id);
+    }
+
+}
+
