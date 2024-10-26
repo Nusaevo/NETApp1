6919e7f40c51304ab925febcb05bd1df125e49d0
Andry <andry@anacle.com>
2024-10-26 15:37:24 +0700

    [Andry] - Add password encryption for env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Core\KTBootstrap;
 use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +17,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $dbUsername = env('DB_ENCRYPTED_USERNAME') ? Crypt::decrypt(env('DB_ENCRYPTED_USERNAME')) : env('DB_USERNAME');
+        $dbPassword = env('DB_ENCRYPTED_PASSWORD') ? Crypt::decrypt(env('DB_ENCRYPTED_PASSWORD')) : env('DB_PASSWORD');
+
+        // Set konfigurasi database secara manual
+        Config::set('database.connections.pgsql.username', $dbUsername);
+        Config::set('database.connections.pgsql.password', $dbPassword);
+        Config::set('database.connections.main.username', $dbUsername);
+        Config::set('database.connections.main.password', $dbPassword);
     }
 
     /**
