27ab82eae66b49b346a013d4f01d9c1b52d4454c
Andry <andry@anacle.com>
2025-02-25 13:58:07 +0700

    [Andry] - Save matl code on matl uom

--- app/Models/TrdJewel1/Master/MatlUom.php
+++ app/Models/TrdJewel1/Master/MatlUom.php
@@ -18,6 +18,14 @@ class MatlUom extends BaseModel
     protected static function boot()
     {
         parent::boot();
+        static::saving(function ($matlUom) {
+            if (!$matlUom->matl_code && $matlUom->matl_id) {
+                $material = Material::find($matlUom->matl_id);
+                if ($material) {
+                    $matlUom->matl_code = $material->code;
+                }
+            }
+        });
     }
 
     protected $fillable = [
