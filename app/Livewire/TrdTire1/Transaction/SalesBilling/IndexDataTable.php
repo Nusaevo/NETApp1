d0f2ae4e56a298378f250ca5f732d30cdcb8300a
adityaeks <mohadityaekosaputra11@gmail.com>
2025-05-06 08:45:34 +0700

    Adit - Update TrdTire1

--- app/Livewire/TrdTire1/Transaction/SalesBilling/IndexDataTable.php
+++ app/Livewire/TrdTire1/Transaction/SalesBilling/IndexDataTable.php
@@ -26,7 +26,7 @@ class IndexDataTable extends BaseDataTableComponent
     {
         return BillingHdr::with(['Partner'])
             ->where('billing_hdrs.tr_type', 'ARB')
-            ->whereIn('billing_hdrs.status_code', [Status::ACTIVE, Status::PRINT, Status::SHIP]);
+            ->whereIn('billing_hdrs.status_code', [Status::ACTIVE, Status::PRINT, Status::OPEN]);
 
     }
 
